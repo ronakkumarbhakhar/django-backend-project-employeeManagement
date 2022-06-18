879c69353f4b6f84de8316f0c3adc6434c33a286
Ronak Kumar <ronakkumar943338@gamil.com>
2022-06-18 17:07:47 +0530

    database setting changed

--- Backend/settings.py
+++ Backend/settings.py
@@ -93,13 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    'new' :{
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': BASE_DIR / 'db',
-        'PASSWORD':'ronak@2002',
-        'HOST': 'localhost',
-        'port' : '3306'
-    }
+    
 }
 
 
