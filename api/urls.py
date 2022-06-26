ef2cfce2fa3bc38130b125bb9226f3ff94dc8dd7
Ronak Kumar <ronakkumar943338@gamil.com>
2022-06-26 14:47:48 +0530

    employee detail view url problem solved

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path,include
-from .views import EmployeeListCreateView ,AttendanceListCreateView, SalaryListCreateView, user_registration_view, CustomAuthToken, user_logout_view
+from .views import EmployeeListCreateView ,AttendanceListCreateView, SalaryListCreateView, user_registration_view, CustomAuthToken, user_logout_view,EmployeeDetailview
 from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('employees/<int:employer_id>/',EmployeeListCreateView.as_view()),
     path('attendance/<int:employer_id>/',AttendanceListCreateView.as_view()),
     path('salary/<int:employer_id>/',SalaryListCreateView.as_view()),
+    path('employeedetail/<int:employee_id>/',EmployeeDetailview.as_view()),
     # path('api-auth/login/',obtain_auth_token,name='login'),
     path('api-auth/login/',CustomAuthToken.as_view(),name='login'),
     path('api-auth/registration/',user_registration_view,name='register'),
     path('api-auth/logout/<int:employer_id>/',user_logout_view,name='logout'),
-    path('api-auth/employeedetail/<int:employee_id>/',user_logout_view,name='employee detail'),
     ]
